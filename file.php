93912bff6bc16e0ccd3716551bda8a53d584f75f
romelectr0 <romelectr0@users.noreply.github.com>
2014-04-28 14:38:02 +0200

    Create file.php

---
+++ file.php
@@ -0,0 +1,13 @@
+<?php #include("element\filesgbd.php"); ?>
+<?php include("element\headerfile.php");?>
+<?php include("element\menu.php"); ?>
+<article>
+	<div class="onefile">
+	<a href="texturepack.zip" style="float:left;"><p class="filename" style="float:left;">download</p></a>
+	<p class="filepower" style="float:left;">2.14k</p>
+	<div style="clear:both;"></div>
+	</div>
+</article>
+<?php include("element\aside.php");?>
+<footer>
+</footer>
