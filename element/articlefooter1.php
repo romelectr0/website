6d53ac4bf439ac873726a5deddf9c475697827aa
romelectr0 <romelectr0@users.noreply.github.com>
2014-04-28 13:58:11 +0200

    Create articlefooter1.php

---
+++ element/articlefooter1.php
@@ -0,0 +1,168 @@
+
+			<?php 
+				echo(2);
+			 ?>">
+			 <p class="pageentour">
+			 <?php 
+					echo(2);
+					?></p></a>
+				</div>
+				<div id="page2nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(3);
+				 ?>"><p class="pageentour"><?php 
+					
+					echo(3);
+					
+					?></p></a>
+				</div>
+				<div id="page3nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(4);
+					 ?>"><p class="pageentour"><?php 
+					
+					echo(4);
+					
+					?></p></a>
+				</div>
+								<div id="page4nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(5);
+					 ?>"><p class="pageentour"><?php 
+					
+					echo(5);
+					
+					?></p></a>
+				</div>
+								<div id="page5nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(6);
+					 ?>"><p class="pageentour"><?php 
+					
+					echo(6);
+					
+					?></p></a>
+				</div>
+								<div id="page6nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(7);
+					 ?>"><p class="pageentour"><?php 
+					
+					echo(7);
+					
+					?></p></a>
+				</div>
+												<div id="page7nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(8);
+					?>"><p class="pageentour"><?php 
+					
+					echo(8);
+					
+					?></p></a>
+				</div>
+												<div id="page8nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(9);
+					 ?>"><p class="pageentour"><?php 
+					
+					echo(9);
+					
+					?></p></a>
+				</div>
+												<div id="page9nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(10);
+					?>"><p class="pageentour"><?php 
+					
+					echo(10);
+					
+					?></p></a>
+				</div>
+												<div id="page10nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(11);
+					
+					?>"><p class="pageentour"><?php 
+					
+					echo(11);
+					
+					?></p></a>
+				</div>
+												<div id="page11nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(12);
+					 ?>"><p class="pageentour"><?php 
+					
+					echo(12);
+					
+					?></p></a>
+				</div>
+												<div id="page12nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(13);
+					 ?>"><p class="pageentour"><?php 
+					
+					echo(13);
+					
+					?></p></a>
+				</div>
+												<div id="page13nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(14);
+					 ?>"><p class="pageentour"><?php 
+					
+					echo(14);
+					
+					?></p></a>
+				</div>
+												<div id="page14nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(15);
+					 ?>"><p class="pageentour"><?php 
+					
+					echo(15);
+					
+					?></p></a>
+				</div>
+																<div id="page15nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(16);
+					 ?>"><p class="pageentour"><?php 
+					
+					echo(16);
+					
+					?></p></a>
+				</div>
+																<div id="page16nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(17);
+				?>"><p class="pageentour"><?php 
+					
+					echo(17);
+					
+					?></p></a>
+				</div>
+																<div id="page17nd" class="allchoice">
+					<a href="page1.php?pages=<?php 
+					
+					echo(18);
+					?>"><p class="pageentour"><?php 
+					
+					echo(18);
+					?>
