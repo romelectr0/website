0a0ac5816dac9b5d2dbcdea6180040ce4affdc11
romelectr0 <romelectr0@users.noreply.github.com>
2014-04-28 14:22:55 +0200

    Create filesgbd.php

---
+++ element/filesgbd.php
@@ -0,0 +1,9 @@
+<?php 
+	$file = array("<p class='text1'>cela fait quelques semaines que je travaille sur ce site je ca conception est toujour en cour et toutes les filepage ne sont pas fini
+	il contient de nombreux bug pour l'instant mais je vais continuer a y travailler les raison de pourquoi j'ai creer ce site : 1. avoir son site sur son propre serveur permer beaucoup plus 
+	de liberte sur le site 2. pouvoir mettre des fichier en telechargement sur son propre serveur permet d'eviter tous les probleme qu'on ces type de site</p>");
+	$filetitle = array("premier article");
+	if (isset($_GET['filepage'])) {
+	$filepage = $_GET['filepage'];
+	}
+ ?>
