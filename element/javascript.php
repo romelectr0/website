0c39c52cd0faa878587e11242718cef470ec5b75
romelectr0 <romelectr0@users.noreply.github.com>
2014-04-28 14:25:16 +0200

    Create javascript.php

---
+++ element/javascript.php
@@ -0,0 +1,14 @@
+<?php /*
+	echo("
+	<script type='text/javascript'>
+	function maFonction() {
+     
+  location.reload(true)
+         
+  }
+ 
+  time = 20000;
+  setTimeout('maFonction();', time);
+	</script>
+	"); */
+?>
