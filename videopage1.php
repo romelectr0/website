e64680de3cbcf634e135310222ded9422dfd8446
romelectr0 <romelectr0@users.noreply.github.com>
2014-04-28 13:32:00 +0200

    Create videopage1.php

---
+++ videopage1.php
@@ -0,0 +1,420 @@
+<?php include('element\videosgbd.php'); ?>
+<?php include("element\headervid.php");?>
+			<?php include("element\menu.php"); ?>
+			<?php  include('element\videotitle.php'); ?>
+				<?php include("element\aside.php");?>
+				</article>
+		<footer>
+			<div class="page">
+			<div id="pagepage" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(2);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(1);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 8);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(2);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(1);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 8);
+					}
+					}
+					?></p></a>
+				</div>
+				<div id="page2nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(3);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(2);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 7);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(3);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(2);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 7);
+					}
+					}
+					?></p></a>
+				</div>
+				<div id="page3nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(4);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(3);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 6);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(4);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(3);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 6);
+					}
+					}
+					?></p></a>
+				</div>
+								<div id="page4nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(5);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(4);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 5);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(5);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(4);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 5);
+					}
+					}
+					?></p></a>
+				</div>
+								<div id="page5nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(6);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(5);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 4);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(6);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(5);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 4);
+					}
+					}
+					?></p></a>
+				</div>
+								<div id="page6nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(7);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(6);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 3);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(7);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(6);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 3);
+					}
+					}
+					?></p></a>
+				</div>
+												<div id="page7nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(8);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(7);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 2);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(8);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(7);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 2);
+					}
+					}
+					?></p></a>
+				</div>
+												<div id="page8nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(9);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(8);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 1);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(9);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(8);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] - 1);
+					}
+					}
+					?></p></a>
+				</div>
+												<div id="page9nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(10);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(9);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage']);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(10);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(9);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage']);
+					}
+					}
+					?></p></a>
+				</div>
+												<div id="page10nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(11);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(10);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 1);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(11);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(10);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 1);
+					}
+					}
+					?></p></a>
+				</div>
+												<div id="page11nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(12);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(11);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 2);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(12);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(11);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 2);
+					}
+					}
+					?></p></a>
+				</div>
+												<div id="page12nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(13);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(12);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 3);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(13);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(12);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 3);
+					}
+					}
+					?></p></a>
+				</div>
+												<div id="page13nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(14);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(13);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 4);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(14);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(13);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 4);
+					}
+					}
+					?></p></a>
+				</div>
+												<div id="page14nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(15);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(14);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 5);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(15);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(14);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 5);
+					}
+					}
+					?></p></a>
+				</div>
+																<div id="page15nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(16);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(15);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 6);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(16);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(15);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 6);
+					}
+					}
+					?></p></a>
+				</div>
+																<div id="page16nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(17);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(16);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 7);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(17);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(16);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 7);
+					}
+					}
+					?></p></a>
+				</div>
+																<div id="page17nd" class="allchoice">
+					<a href="videopage1.php?videopage=<?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(18);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(17);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 8);
+					}
+					} ?>"><p class="pageentour"><?php if(isset($_GET['videopage'])) {
+					if ($_GET['videopage'] == 1){
+					echo(18);
+					}
+					if ($_GET['videopage'] > 1 && $_GET['videopage'] <= 9) {
+					echo(17);
+					}
+					if ($_GET['videopage'] > 9) {
+						echo($_GET['videopage'] + 8);
+					}
+					}
+					?></p></a>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
