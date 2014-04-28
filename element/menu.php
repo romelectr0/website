0952d0f57383fb2dff2555df9861d64ecb108582
romelectr0 <romelectr0@users.noreply.github.com>
2014-04-28 14:25:36 +0200

    Create menu.php

---
+++ element/menu.php
@@ -0,0 +1,21 @@
+			<div id="menu">
+				<div id="choice1">
+					<a href="page1.php?pages=1"><p id="textchoice1">Home</p></a>
+				</div>
+				<div id="choice2" class="choice">
+					<a href="videopage1.php?videopage=1"><p id="textchoice2">Video</p></a>
+				</div>
+				<div id="choice3" class="choice">
+					<a href="file.php?filepage=1"><p id="textchoice3">File</p></a>
+				</div>
+				<div id="choice4" class="choice">
+					<p id="textchoice4">Schematic , Picture</p>
+				</div>
+				<form id="finder" action="find.php" method="POST">
+					<input id="cherche" type="text" name="nom"  x-webkit-speech/>
+					<input id="valide" type="Submit" value="search" name="fonction"/>
+				</form>
+				<div id="choice5">
+					<a href="create.php?createpage=1"><p id="textechoice5">Creation php</p></a>
+				</div>
+			</div>
