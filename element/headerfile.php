3a9094067aa4f621a143e013b41fe6c3246ad9c2
romelectr0 <romelectr0@users.noreply.github.com>
2014-04-28 14:24:17 +0200

    Create headerfile.php

---
+++ element/headerfile.php
@@ -0,0 +1,19 @@
+
+<html>
+	<head>
+		<?php include("javascript.php"); ?>
+		<link rel="stylesheet" href="element\<?php 
+		echo("style.css");
+		?>"/>
+		<title>romelectr0's webstie</title>
+	</head>
+	<body>
+	<div id="bloc_page">
+		<header>
+			<a href="https://www.facebook.com/pages/Incroyable-innovation/378040715591314?ref=hl"><img src="element\facebookimage.jpg" alt="logo facebook" id="logo"/></a>
+			<a href="https://www.facebook.com/pages/Incroyable-innovation/378040715591314?ref=hl"><p id="facebook">aller voir ma page facebook</p></a>
+			<img src="element\hack skull.jpg" alt="banniere" id="image_principale"/>
+			<a href="http://www.youtube.com/user/incroyableinnovation"><img src="element\youtubeimage.jpg" alt="youtube" id="imageyoutube"/></a>
+			<a href="http://www.youtube.com/user/incroyableinnovation"><p id="youtubetext">aller sur ma chaine youtube</p></a>
+		</header>
+			<section>
